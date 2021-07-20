b17f6e7f73aacbba6d0d31f4d3a0d72185ba3ba6
Daniel Berghold <microsoft@berghold.cc>
2021-07-20 18:48:30 +0200

    Added virtual method of subcontract filtering to ContractSchemaProcessor.

--- Inspiring.Json.NSwag/ContractSchemaProcessor.cs
+++ Inspiring.Json.NSwag/ContractSchemaProcessor.cs
@@ -15,26 +15,31 @@ namespace Inspiring.Json.NSwag {
         public void Process(SchemaProcessorContext context) {
             if (_registry.IsPolymorphic(context.Type, out ContractTypeHierarchy? hierarchy)) {
                 if (hierarchy!.BaseContractType == context.Type) {
-                    var discriminator = new OpenApiDiscriminator {
+                    context.Schema.DiscriminatorObject = new OpenApiDiscriminator {
                         PropertyName = hierarchy.DiscriminatorName
                     };
 
-                    foreach (SubcontractType subtype in hierarchy.Subcontracts) {
-                        discriminator.Mapping.Add(subtype.DiscriminatorValue, ProcessSubtype(subtype.Type, context.Schema));
-                    }
-
-                    context.Schema.DiscriminatorObject = discriminator;
                     context.Schema.Properties[hierarchy.DiscriminatorName] = new JsonSchemaProperty {
                         Type = JsonObjectType.String,
                         IsRequired = true
                     };
+
+                    foreach (SubcontractType subtype in hierarchy.Subcontracts)
+                        ProcessContractSubtype(context, subtype);
                 }
             }
+        }
 
-            JsonSchema ProcessSubtype(Type t, JsonSchema parent) {
-                
+        protected virtual void ProcessContractSubtype(SchemaProcessorContext context,  SubcontractType subtype) {
+            context
+                .Schema
+                .DiscriminatorObject
+                .Mapping
+                .Add(subtype.DiscriminatorValue, createSubtypeSchema(subtype.Type, context.Schema));
+
+            JsonSchema createSubtypeSchema(Type t, JsonSchema parent) {
                 JsonSchema s = context.Resolver.HasSchema(t, false) ?
-                    context.Resolver.GetSchema(t, false) : 
+                    context.Resolver.GetSchema(t, false) :
                     context.Generator.Generate(t, context.Resolver);
 
                 s.AllOf.Add(new JsonSchema { Reference = parent });
